a8ea98a50ac605f2d057ce485220ab931ce8d2ec
Grigory Chernyshev <systray@yandex.ru>
2017-11-30 14:34:03 +0100

    Fix tmux env refresh function.

--- .zshrc.d/refresh-tmux-env.zsh
+++ .zshrc.d/refresh-tmux-env.zsh
@@ -2,14 +2,17 @@
 # Refresh environment variables from tmux and exports them.
 #
 if [ -n "$TMUX" ]; then
-  function refresh {
-    export $(tmux show-environment | grep "^SSH_AUTH_SOCK")
-    export $(tmux show-environment | grep "^DISPLAY")
-  }
+    function refresh {
+        sshauth=$(tmux show-environment | grep "^SSH_AUTH_SOCK")
+        if [ $sshauth ]; then
+            export $sshauth
+        fi
+        display=$(tmux show-environment | grep "^DISPLAY")
+        if [ $display ]; then
+            export $display
+        fi
+    }
 else
-  function refresh { }
+    function refresh { }
 fi
 
-function preexec {
-    refresh
-}
