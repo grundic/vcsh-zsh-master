7d69bc40bea3c5df1fe9d204e8e70dc7dfc77304
Grigory Chernyshev <systray@yandex.ru>
2018-03-26 10:11:46 +0200

    Add pless for color less, add fzf.

--- .zshrc.d/aliases.zsh
+++ .zshrc.d/aliases.zsh
@@ -1,3 +1,6 @@
 
 # Force tmux to assume that the shell is using 256 color terminal.
 alias tmux="tmux -2"
+
+# color less using pygments
+pless() { pygmentize -f terminal256 -g -P style=monokai $* | less -FiXRM }
