47cf7ab6a7e6ac5cb3ac820c6ebf2baf85828d15
Grigory Chernyshev <systray@yandex.ru>
2018-11-19 14:15:12 +0100

    Add date aliases.

--- .zshrc.d/aliases.zsh
+++ .zshrc.d/aliases.zsh
@@ -4,3 +4,29 @@ alias tmux="tmux -2"
 
 # color less using pygments
 pless() { pygmentize -f terminal256 -g -P style=monokai $* | less -FiXRM }
+
+# date aliases
+
+epoch() {
+  date +"%s"
+}
+
+day() {
+  date -d "@$(($1))"
+}
+
+day-milli() {
+  date -d "@$(($1 / 1000))"
+}
+
+day-micro() {
+  date -d "@$(($1 / 1000000))"
+}
+
+day-nano() {
+  date -d "@$(($1 / 1000000000))"
+}
+
+day-diff() {
+  $(( ($(date -d "$1" +%s) - $(date -d "$2" +%s) )/(60*60*24) ))
+}
