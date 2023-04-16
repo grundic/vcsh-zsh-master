8a2ffb70cbb5e52d7d594d6e32eeed139ba89cc1
Grigory Chernyshev <systray@yandex.ru>
2023-04-16 09:47:34 +0200

    Switch to neovim

--- .zshrc.d/aliases.zsh
+++ .zshrc.d/aliases.zsh
@@ -1,4 +1,6 @@
 
+alias vim="nvim"
+
 # Force tmux to assume that the shell is using 256 color terminal.
 alias tmux="tmux -2"
 
--- .zshrc.d/env.zsh
+++ .zshrc.d/env.zsh
@@ -1,4 +1,4 @@
-export VISUAL=vim
+export VISUAL=nvim
 export EDITOR="$VISUAL"
 export LESS='-F -g -i -M -R -S -w -X -z-4'
 
